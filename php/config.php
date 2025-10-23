6d865e6724bc86467b38ec6d5ab90fdc39bb27a2
RajNilkar <rajitnilkar28@gmail.com>
2025-10-22 20:13:42 -0600

    Save current PHP work before merging main

---
+++ php/config.php
@@ -0,0 +1,18 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "bookburners_db";
+
+//Create connection
+$conn = new mysqli(($servername), ($username), ($password), ($db_name));
+
+//Check connection
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+else
+{
+    echo "Connected successfully";
+}
+?>
