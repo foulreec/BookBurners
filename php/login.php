36100f39ba0fe9fec244acbdb9ef5497505741c5
RajNilkar <rajitnilkar28@gmail.com>
2025-12-08 10:37:40 -0700

    Fixed Login Feature
    Added a profile page, a logout button, and some php functionality to check if the user is logged in or not.

--- php/login.php
+++ php/login.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+session_start();
+
 require_once __DIR__ . '/config.php';
 
 $conn = new mysqli($servername, $username, $password, $db_name);
@@ -7,22 +9,30 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
+
 //Handle login form submission
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
     $email = $_POST['email'];
+    $username = $_POST['username'];
     $password = $_POST['user_password'];
 
-    $stmt = $conn->prepare("SELECT user_password FROM users WHERE email = ?");
+    $stmt = $conn->prepare("SELECT user_id, username, user_password FROM users WHERE email = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $stmt->store_result();
 
     if($stmt->num_rows == 1){
-        $stmt->bind_result($hashed_password);
+        $stmt->bind_result($db_user_id, $username, $hashed_password);
         $stmt->fetch();
 
-        if(password_verify($password, $hashed_password)){
+        if($db_user_id && $username && password_verify($password, $hashed_password)){
+
+            $_SESSION['user_id'] = $db_user_id;
+            $_SESSION['username'] = $username;
+            $_SESSION['email'] = $email;
+
             echo "Login successful";
         } else {
             echo "Invalid password";
